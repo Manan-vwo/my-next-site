52d29b9054a64ba6ef5b58706c8d118ba65abe91
Manan Singh <manan.singh@vwo.com>
2025-12-16 19:50:04 +0530

    new sc test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-// import { VWOScript } from "vwo-smartcode-nextjs";
-import { VWOScript } from "./VWOsc";
+import { VWOScript } from "vwo-smartcode-nextjs";
+// import { VWOScript } from "./VWOsc";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
