67dba0b5277a3f5117a0f97ede9462652827464f
Manan Singh <manan.singh@vwo.com>
2026-03-26 23:32:30 +0530

    Update vwo-smartcode-nextjs to version 1.4.2 and adjust package references

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* <VWOScript accountId="1120758" type="ASYNC" /> */}
         <VWOScript accountId="1120758" type="ASYNC" />
       </head>
       <body
