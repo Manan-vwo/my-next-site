23648211667a84d56971bb0b2e206fa40440a1ce
Manan Singh <manan.singh@vwo.com>
2026-09-08 16:22:15 +0530

    Update dependencies to replace vwo-smartcode-nextjs with wingify-smartcode-nextjs, and upgrade @emnapi/runtime to version 1.11.2. Modify layout.tsx to use WingifyScript with updated accountId and type.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import { VWOScript } from "vwo-smartcode-nextjs";
+import { WingifyScript } from "wingify-smartcode-nextjs";
+// import "./globals.css";
+// import { VWOScript } from "vwo-smartcode-nextjs";
 // import { VWOScript } from "./VWOsc";
 
 const geistSans = Geist({
@@ -28,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <VWOScript accountId="1120758" type="ASYNC" />
+        <WingifyScript accountId="1276768" type="SYNC" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
