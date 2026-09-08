04a534e3a35c6b313844eea00d0fd151727b8a74
Manan Singh <manan.singh@vwo.com>
2026-09-08 16:25:44 +0530

    Enable global styles by uncommenting the import statement in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { WingifyScript } from "wingify-smartcode-nextjs";
-// import "./globals.css";
+import "./globals.css";
 // import { VWOScript } from "vwo-smartcode-nextjs";
 // import { VWOScript } from "./VWOsc";
 
